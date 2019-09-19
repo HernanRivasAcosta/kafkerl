a0918e278ba0156fa4798f3f93fa8c64917e0e9f
Ang Li <ali@tigerconnect.com>
2019-09-19 18:49:41 +0800

    fix

--- src/kafkerl_buffer.erl
+++ src/kafkerl_buffer.erl
@@ -4,7 +4,8 @@
 -behavior(gen_server).
 
 %% API
--export([start_link/0, init/1, create_buffer/2, handle_call/3, start_link/1, create_buffer/1]).
+-export([start_link/0, init/1, create_buffer/2, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3,
+    start_link/1, create_buffer/1]).
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
@@ -31,4 +32,14 @@ handle_call({create_buffer, Name}, _From, State) ->
     lager:debug("buffer ~p created ~p", [Name, Res]),
     {reply, ok, State}.
 
+handle_cast(_Request, State) ->
+    {noreply, State}.
 
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
